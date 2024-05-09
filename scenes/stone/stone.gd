3d9a01aa4655bb3a36142f59b62514575b10adab
Daniil Smoliakov <danonofficial@gmail.com>
2024-05-09 18:56:38 +0100

    Added throw strength code

--- scenes/stone/stone.gd
+++ scenes/stone/stone.gd
@@ -1,23 +1,20 @@
 extends RigidBody2D
 class_name Stone
 
-#(dsmoliakov): move object to player node and enable is_in_hand, so it follows after player 
-var is_in_hand = false
 var is_player_near = false
 var can_player_throw = false
 var follow_target : Node2D = null
 var is_follow_active : bool = false
 
-@export var throw_power : float = 450
+var throw_charge_timer : Timer
+@export var max_time_throw_charge = 2.0
+@export var min_throw_power : float = 100
+@export var max_throw_power : float = 450
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	throw_charge_timer = Timer.new()
+	add_child(throw_charge_timer)
 	pass # Replace with function body.
-
-
-func set_in_hand(state):
-	is_in_hand = state
-	freeze = state
-		
 
 
 func _physics_process(delta: float) -> void:
@@ -37,13 +34,18 @@ func set_follow_active(is_active : bool, target : Node2D):
 
 
 func check_input():
-	if Input.is_action_just_pressed("take_stone"):
-		if is_player_near and !can_player_throw:
-			set_follow_active(true, get_tree().get_first_node_in_group("player"))
-			can_player_throw = true
-			(follow_target as Player).is_stone_in_hands = true
-		elif can_player_throw:
+	if Input.is_action_just_released("take_stone"):
+		if !can_player_throw:
+			if is_player_near:
+				set_follow_active(true, get_tree().get_first_node_in_group("player"))
+				can_player_throw = true
+				(follow_target as Player).is_stone_in_hands = true
+		else:
 			throw()
+	
+	if Input.is_action_just_pressed("take_stone") and can_player_throw:
+		throw_charge_timer.start(max_time_throw_charge)
+			
 	
 
 
@@ -64,11 +66,13 @@ func throw():
 	is_follow_active = false
 	can_player_throw = false
 	(follow_target as Player).is_stone_in_hands = false
-	apply_central_impulse(Vector2(1 * sign(follow_target.face_direction),-0.8) * throw_power)
+	var normalized_time = 1 - throw_charge_timer.time_left / max_time_throw_charge
+	var final_throw_power = lerp(min_throw_power, max_throw_power, normalized_time)
+	apply_central_impulse(Vector2(1 * sign(follow_target.face_direction),-0.8) * final_throw_power)
 	
 
 func _on_pickup_area_body_entered(body : Node2D):
-	if body.is_in_group("player") and !is_in_hand:
+	if body.is_in_group("player"):
 		is_player_near = true
 
 
