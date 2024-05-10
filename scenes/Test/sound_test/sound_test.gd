ed995acd3ab7301e29522aea5850dee3f48132ef
AntOnion <sabatonik@mail.ru>
2024-05-10 20:20:46 +0300

    sounds added * audio layers added

--- scenes/Test/sound_test/sound_test.gd
+++ scenes/Test/sound_test/sound_test.gd
@@ -1,6 +1,7 @@
 extends Node
 
-@onready var audio_stream_player = $AudioStreamPlayer
+@onready var audio_stream_player = $LevelFinish
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
