3544885e69905cc96e3037622dcd2a2edce6fac4
Daniil Smoliakov <danonofficial@gmail.com>
2024-05-11 10:25:39 +0100

    Add soft light for spikes

--- scenes/light_fearing_spike/light_fearing_spike.gd
+++ scenes/light_fearing_spike/light_fearing_spike.gd
@@ -14,6 +14,11 @@ var appearance_timer : Timer = null
 @export var dissapearence_time = 2.0
 
 @onready var sprite : Sprite2D = $Sprite2D
+
+var light_timer : Timer
+@export var light_timer_appearance : float = 1.0
+var is_point_light_enabled = false
+var is_active = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	dissapearence_timer = Timer.new()
@@ -23,6 +28,10 @@ func _ready():
 	appearance_timer.one_shot = true
 	add_child(appearance_timer)
 	light_stone = (get_tree().get_first_node_in_group("stone") as Stone)
+	point_light.energy = 0
+	light_timer = Timer.new()
+	light_timer.one_shot = true
+	add_child(light_timer)
 	pass # Replace with function body.
 
 
@@ -34,6 +43,8 @@ func stone_entering_processing(body: Node2D):
 	var normalized_time =  1 - time_left_on_appearance / dissapearence_time
 	dissapearence_timer.start(normalized_time * dissapearence_time)
 
+func set_point_light(state):
+	is_point_light_enabled = true
 	
 func stone_exiting_processing(body: Node2D):
 	if !body.is_in_group("stone"):
@@ -42,23 +53,27 @@ func stone_exiting_processing(body: Node2D):
 	var time_left_on_dissapearance = dissapearence_timer.time_left
 	var normalized_time =  1 - time_left_on_dissapearance / appearance_time
 	appearance_timer.start(normalized_time * appearance_time)
-	point_light.visible = true
+
 
 
 func process_collision_with_player(body : Node2D):
-	
-	if body.is_in_group("player") and !in_zone_of_light and !point_light.visible : #(dsmoliakov): totally wrong
+	if body.is_in_group("player") and !in_zone_of_light and appearance_timer.time_left < 0.001:
 		(body as Player).respawn()
 		(get_tree().get_first_node_in_group("stone") as Stone).respawn()
+		set_point_light(true)
+		light_timer.start(light_timer_appearance)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if in_zone_of_light:
 		var time_left_on_dissapearance = dissapearence_timer.time_left
 		sprite.self_modulate.a = time_left_on_dissapearance / dissapearence_time
+			
 	else:
 		var time_left_on_appearance = appearance_timer.time_left
 		sprite.self_modulate.a = (1 - time_left_on_appearance / appearance_time)
-		if time_left_on_appearance <= 0.0:
-			point_light.visible = false
+		
+	if is_point_light_enabled and light_timer.time_left > 0 and point_light.energy < 1:
+		var normalized_time = light_timer.time_left / light_timer_appearance
+		point_light.energy = 1 - normalized_time
 
--- scenes/spike/spike.gd
+++ scenes/spike/spike.gd
@@ -4,8 +4,16 @@ class_name Spike
 @onready var point_light = $PointLight2D
 @onready var death_area = $Area2D
 
+var is_point_light_enabled = false
+
+var light_timer : Timer
+@export var light_timer_appearance : float = 1.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	light_timer = Timer.new()
+	light_timer.one_shot = true
+	add_child(light_timer)
+	point_light.energy = 0
 	pass # Replace with function body.
 
 func process_collision_with_player(body : Node2D):
@@ -13,12 +21,22 @@ func process_collision_with_player(body : Node2D):
 		(body as Player).respawn()
 		(get_tree().get_first_node_in_group("stone") as Stone).respawn()
 		set_point_light(true)
-	
-	
+		light_timer.start(light_timer_appearance)
+	if body.is_in_group("stone"):
+		var current_velocity = (body as RigidBody2D).linear_velocity
+		var direction = -current_velocity
+		if (body as RigidBody2D).linear_velocity.length() < 0.1: #(dsmoliakov): well, working so far
+			var player_pos = (get_tree().get_first_node_in_group("stone") as Node2D).position
+			direction = player_pos - body.position
+		(body as Stone).launch(direction)
 
+	
 func set_point_light(state):
-	point_light.visible = state
+	is_point_light_enabled = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if is_point_light_enabled and light_timer.time_left > 0:
+		var normalized_time = light_timer.time_left / light_timer_appearance
+		point_light.energy = 1 - normalized_time
 	pass
