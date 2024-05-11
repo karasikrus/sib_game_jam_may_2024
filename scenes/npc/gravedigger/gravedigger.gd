53a9d40ec19eb25c7992b71d5953cec343afccc0
Daniil Smoliakov <danonofficial@gmail.com>
2024-05-11 16:46:24 +0100

    gravedigger

--- scenes/npc/gravedigger/gravedigger.gd
+++ scenes/npc/gravedigger/gravedigger.gd
@@ -3,16 +3,19 @@ extends Node2D
 var dialog_played : bool = false
 var dialog_playing : bool = false
 
-@export var dialog = "timeline_test"
+@export var dialog = "timeline_1"
 
+@onready var player = $AnimationPlayer
 var is_player_near = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#player.play("idle")
 	pass # Replace with function body.
 	
 func _on_dialog_area_body_entered(body : Node2D):
 	if body.is_in_group("player"):
 		is_player_near = true
+	
 
 func _on_dialog_area_body_exited(body):
 	if body.is_in_group("player"):
@@ -21,7 +24,8 @@ func _on_dialog_area_body_exited(body):
 func play_dialog():
 	Dialogic.start(dialog)
 
-func check_input():	if Input.is_action_just_released("start_dialog"):
+func check_input():
+	if Input.is_action_just_released("start_dialog"):
 		if is_player_near:
 			var player = (get_tree().get_first_node_in_group("player") as Player)
 			player.freeze_movement()
@@ -32,4 +36,5 @@ func check_input():	if Input.is_action_just_released("start_dialog"):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	check_input()
+	player.play("idle")
 	pass
